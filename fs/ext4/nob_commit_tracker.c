b1fea37b00b6fa27949af581fb69b075e60e908f
anonymousresearcher01 <mdh38112@sogang.ac.kr>
2026-03-23 03:02:36 +0000

    add nob_commit_tracker

---
+++ fs/ext4/nob_commit_tracker.c
@@ -0,0 +1,81 @@
+#include "nob_commit_tracker.h"
+#include <linux/printk.h>
+#include <linux/slab.h>
+
+
+struct nob_commit_tracker g_nob_tracker;
+
+void nob_tracker_list(void){
+    hash_init(g_nob_tracker.pending);
+    hash_init(g_nob_tracker.committed);
+    spin_lock_init(&g_nob_tracker.spinlock);
+    pr_info("NobLSM's commit tracker init\n");
+}
+
+int nob_pending_add(unsigned long ino){
+    struct nob_pending_entry *entry;
+    entry = kmalloc(sizeof(*e), GFP_KERNEL);
+    if (!entry)
+        return -ENOMEM;
+    
+    entry->ino = ino;
+    spin_lock(&g_nob_tracker.spinlock);
+    hash_add(g_nob_tracker.pending, &entry->node, ino);
+    spin_unlock(&g_nob_tracker.spinlock);
+    return 0;
+}
+
+bool nob_is_committed(unsigned long ino){
+    struct nob_committed_entry * entry;
+    bool found = false;
+
+    spin_lock(&g_nob_tracker.spinlock);
+    hash_for_each_possible(g_nob_tracker.committed, entry, node, ino){
+        if (entry->ino == ino) {
+            found = true;
+            break;
+        }
+    }
+    spin_unlock(&g_nob_tracker.spinlock);
+    return found;
+}
+void nob_move_to_committed(unsigned long ino){
+    struct nob_pending_entry *pe;
+    struct nob_committed_entry *ce;
+    
+
+    spin_lock(&g_nob_tracker.spinlock);
+    hash_for_each_possible(g_nob_tracker.pending, pe, node, ino) {
+        if (pe->ino == ino){
+            hash_del(&pe->node);
+            kfree(pe);
+            break;
+        }
+    }
+    spin_unlock(&g_nob_tracker.spinlock);
+    ce = kmalloc(sizeof(*ce), GFP_KERNEL);
+    if (!ce)
+        return;
+    
+    ce->ino = ino;
+    spin_lock(&g_nob_tracker.spinlock);
+    hash_add(g_nob_tracker.committed, &ce->node, ino);
+    spin_unlock(&g_nob_tracker.spinlock);
+
+}
+
+void nob_commiited_erase(unsigned long ino)
+{
+    struct nob_committed_entry * entry;
+    spin_lock(&g_nob_tracker.spinlock);
+    hash_for_each_possible(g_nob_tracker.committed, entry, node, ino) {
+        if (entry->ino == ino){
+            hash_del(&entry->node);
+            kfree(entry);
+            break;
+        }
+    }
+    spin_unlock(&g_nob_tracker.spinlock);
+}
+
+
---
+++ fs/ext4/nob_commit_tracker.h
@@ -0,0 +1,36 @@
+/* nob_commit_tracker.h
+*/
+#ifndef _NOB_COMMIT_TRACKER_H
+#define _NOB_COMMIT_TRACKER_H
+
+#include <linux/types.h>
+#include <linux/spinlock.h>
+#include <linux/hashtable.h>
+
+#define NOT_HASH_BITS 8
+
+struct nob_pending_entry{
+    unsigned long ino;
+    struct hlist_node node;
+};
+
+struct nob_committed_entry {
+    unsigned long ino;
+    struct hlist_node node;
+};
+
+struct nob_commit_tracker {
+    DECLARE_HASHTABLE(pending, NOB_HASH_BITS);
+    DECLARE_HASHTABLE(committed, NOB_HASH_BITS);
+    spinlock_t spinlock;
+};
+
+extern struct nob_commit_tracker g_nob_tracker;
+
+void nob_tracker_init(void);
+int nob_pending_add(unsigned long ino);
+bool nob_is_committed(unsigned long ino);
+void nob_move_to_committed(unsigned long ino);
+void nob_commiited_erase(unsigned long ino);
+
+#endif /* _NOB_COMMIT_TRACKER_H */
